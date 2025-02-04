8da36cab2e2a0e16b6a7c25c6274013e07d3be7f
Brannon King <countprimes@gmail.com>
2025-02-04 17:04:12 -0500

    basic ortools wrapper for displib

---
+++ program/Models.cs
@@ -0,0 +1,195 @@
+namespace program;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public class Resource
+{
+    [JsonPropertyName("resource")]
+    public string Name { get; set; }
+    
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public int ReleaseTime { get; set; } = 0;
+
+    public override bool Equals(object obj) => obj is Resource other && Name == other.Name;
+    public override int GetHashCode() => Name.GetHashCode();
+}
+
+public class Segment
+{
+    public int StartLb { get; set; } = 0;
+    public int StartUb { get; set; } = -1;
+    public int MinDuration { get; set; } = 0;
+    public List<Resource> Resources { get; set; } = new();
+    public List<int> Successors { get; set; } = new();
+    public List<int> Predecessors { get; set; } = new();
+}
+
+public class Objective
+{
+    [JsonPropertyName("type")]
+    public string ObjectiveType { get; set; } = "op_delay";
+    public int Train { get; set; }
+    public int Operation { get; set; }
+    public int Threshold { get; set; } = 0;
+    public int Coeff { get; set; } = 0;
+    public int Increment { get; set; } = 0;
+}
+
+public class Event : IComparable<Event>
+{
+    public int Operation { get; set; }
+    public int Time { get; set; }
+    public int Train { get; set; }
+
+    public int CompareTo(Event other) => Time.CompareTo(other.Time);
+    public override bool Equals(object obj) => obj is Event e && Train == e.Train && Operation == e.Operation;
+    public override int GetHashCode() => HashCode.Combine(Train, Operation);
+}
+
+public class Problem
+{
+    public List<List<Segment>> Trains { get; set; } = new();
+    
+    [JsonPropertyName("objective")]
+    public List<Objective> Objectives { get; set; } = new();
+    
+    public string Name { get; set; } = "";
+    
+    public int NumTrains => Trains.Count;
+
+    public static Problem LoadFromFile(string filename, bool noWarnings = false)
+    {
+        string text = File.ReadAllText(filename);
+        var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower };
+        var problem = JsonSerializer.Deserialize<Problem>(text, options);
+        problem.Name = Path.GetFileNameWithoutExtension(filename);
+        problem.BuildPredecessors();
+        return problem;
+    }
+
+    public void BuildPredecessors()
+    {
+        foreach (var train in Trains)
+        {
+            for (int i = 0; i < train.Count; i++)
+            {
+                foreach (var successor in train[i].Successors)
+                {
+                    train[successor].Predecessors.Add(i);
+                }
+            }
+        }
+    }
+
+    public Dictionary<string, List<(int, int, int, int)>> FindResourceChains()
+    {
+        var result = new Dictionary<string, List<(int, int, int, int)>>();
+        for (int t = 0; t < Trains.Count; t++)
+        {
+            var train = Trains[t];
+            for (int v = 0; v < train.Count; v++)
+            {
+                foreach (var resource in train[v].Resources)
+                {
+                    int u = FindStartOfChain(train, v, resource.Name);
+                    if (!result.ContainsKey(resource.Name))
+                    {
+                        result[resource.Name] = new List<(int, int, int, int)>();
+                    }
+                    result[resource.Name].Add((t, u, v, resource.ReleaseTime));
+                }
+            }
+        }
+        return result;
+    }
+
+    private static int FindStartOfChain(List<Segment> train, int v, string resourceName)
+    {
+        while (v > 0)
+        {
+            if (train[v].Predecessors.Count != 1)
+                return v;
+            var u = train[v].Predecessors[0];
+            if (train[u].Resources.All(r => r.Name != resourceName) || train[u].Successors.Count > 1)
+                return v;
+            v = u;
+        }
+        return v;
+    }
+
+    public int FindUpperBound()
+    {
+        var result = 0;
+        for (int t = 0; t < Trains.Count; t++)
+        {
+            var ub = 0;
+            for (int u = 0; u < Trains[t].Count; u++)
+            {
+                var segment = Trains[t][u];
+                ub = Math.Max(ub, segment.StartUb);
+                ub += segment.MinDuration;
+                if (segment.Resources.Count > 0) {
+                    ub += segment.Resources.Max(sr => sr.ReleaseTime);
+                }
+            }
+
+            result += ub;
+        }
+        return result;
+    }
+
+    public List<Dictionary<int, int>> FindShortestPaths()
+    {
+        var dists = new List<Dictionary<int, int>>();
+
+        for (int t = 0; t < Trains.Count; t++)
+        {
+            var train = Trains[t];
+            var dist = new Dictionary<int, int> { { 0, 0 } };
+            dists.Add(dist);
+
+            for (int u = 0; u < train.Count; u++)
+            {
+                var segment = train[u];
+                foreach (var v in segment.Successors)
+                {
+                    int lb = train[v].StartLb;
+                    int toV = Math.Max(dist[u] + segment.MinDuration, lb);
+                    if (!dist.ContainsKey(v) || dist[v] > toV)
+                    {
+                        dist[v] = toV;
+                    }
+                }
+            }
+        }
+
+        return dists;
+    }
+}
+
+public class Solution
+{
+    public List<Event> Events { get; set; } = new();
+    public int ObjectiveValue { get; set; }
+
+    public void WriteToFile(string filename)
+    {
+        var options = new JsonSerializerOptions { WriteIndented = true, PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower};
+        string json = JsonSerializer.Serialize(this, options);
+        var dir = Path.GetDirectoryName(filename);
+        Directory.CreateDirectory(dir);
+        File.WriteAllText(filename, json);
+    }
+
+    public static Solution LoadFromFile(string filename)
+    {
+        string text = File.ReadAllText(filename);
+        var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower };
+        return JsonSerializer.Deserialize<Solution>(text, options)!;
+    }
+}
---
+++ program/Program.cs
@@ -0,0 +1,214 @@
+﻿namespace program;
+
+using Google.OrTools.Sat;
+using TqdmSharp;
+
+class Program
+{
+    static (CpModel model, List<List<IntVar>> outerStarts, List<Dictionary<(int, int), LinearExpr>> outerEnablers) BuildCpModel(Problem problem, int maxGap)
+    {
+        int upperBound = problem.FindUpperBound();
+        var distances = problem.FindShortestPaths();
+        
+        var model = new CpModel();
+        model.Model.Name = problem.Name;
+        
+        var outerEnablers = new List<Dictionary<(int, int), LinearExpr>>();
+        var outerStarts = new List<List<IntVar>>();
+        
+        for (int t = 0; t < problem.Trains.Count; t++)
+        {
+            var train = problem.Trains[t];
+            var starts = new List<IntVar>();
+            for (int u = 0; u < train.Count; u++)
+            {
+                int lb = distances[t][u];
+                int ub = train[u].StartUb < 0 ? upperBound : train[u].StartUb;
+                starts.Add(model.NewIntVar(lb, ub, $"s_{t}_{u}"));
+            }
+            outerStarts.Add(starts);
+            outerEnablers.Add(new Dictionary<(int, int), LinearExpr>());
+
+            var needsEnablers = -1;
+            for (int u = 0; u < train.Count; u++)
+            {
+                if (needsEnablers < 0 && train[u].Successors.Count > 1)
+                {
+                    needsEnablers = u;
+                }
+                foreach (var v in train[u].Successors)
+                {
+                    int md = train[u].MinDuration;
+                    var added = model.Add(starts[v] >= starts[u] + md);
+                    if (needsEnablers >= 0)
+                    {
+                        var enabler = model.NewBoolVar($"b_{u}_{v}");
+                        outerEnablers[t][(u, v)] = enabler;
+                        added.OnlyEnforceIf(enabler);
+                    }
+                }
+            }
+
+            if (needsEnablers >= 0)
+            {
+                for (var u = needsEnablers; u < train.Count; u++)
+                {
+                    LinearExpr predecessors = model.NewConstant(1);
+                    if (u > 0)
+                    {
+                        var preds = train[u].Predecessors.Select(p => 
+                            outerEnablers[t].GetValueOrDefault((p, u), model.NewConstant(1))).ToList();
+                        predecessors = LinearExpr.Sum(preds);
+                        if (preds.Count > 1)
+                        {
+                            model.Add(predecessors <= 1);
+                        }
+                    }
+                    LinearExpr successors = model.NewConstant(1);
+                    if (u + 1 < train.Count)
+                    {
+                        var succs = train[u].Successors.Select(s => 
+                            outerEnablers[t].GetValueOrDefault((u, s), model.NewConstant(1))).ToList();
+                        successors = LinearExpr.Sum(succs);
+                        if (succs.Count > 1)
+                        {
+                            model.Add(successors <= 1);
+                        }
+                    }
+                    model.Add(predecessors == successors);
+                }
+            }
+        }
+
+        var tToChains = problem.FindResourceChains();
+        var disjunctions = 0;
+        foreach (var chains in Tqdm.Wrap(tToChains.Values))
+        {
+            for (int idx = 0; idx < chains.Count; idx++)
+            {
+                var (t1, u1, v1t, rt1) = chains[idx];
+                var v1succs = new List<int>{-1};
+                if (problem.Trains[t1][v1t].Successors.Count > 0)
+                    v1succs = problem.Trains[t1][v1t].Successors;
+                var u1s = outerStarts[t1][u1];
+                rt1 = Math.Max(1, rt1);
+                foreach (var (t2, u2, v2t, rt2) in chains.Skip(idx + 1))
+                {
+                    BoolVar ch = null;
+                    var v2succs = new List<int>{-1};
+                    if (problem.Trains[t2][v2t].Successors.Count > 0)
+                        v2succs = problem.Trains[t2][v2t].Successors;
+                    var u2s = outerStarts[t2][u2];
+                    var rt2a = Math.Max(1, rt2);
+                    
+                    foreach (var v1 in v1succs)
+                    {
+                        foreach (var v2 in v2succs)
+                        {
+                            LinearExpr v1s = v1 < 0 ? u1s + problem.Trains[t1][v1t].MinDuration : outerStarts[t1][v1];
+                            var en1 = (ILiteral)outerEnablers[t1].GetValueOrDefault((v1t, v1), null);
+                            var oei1 = new List<ILiteral>();
+                            if (en1 != null)
+                                oei1.Add(en1);
+
+                            LinearExpr v2s = v2 < 0 ? u2s + problem.Trains[t2][v2t].MinDuration : outerStarts[t2][v2];
+                            var en2 = (ILiteral)outerEnablers[t2].GetValueOrDefault((v2t, v2), null);
+                            var oei2 = new List<ILiteral>();
+                            if (en2 != null)
+                                oei2.Add(en2);
+                            if (ch == null)
+                                ch = model.NewBoolVar($"c_{t1}_{u1}_{t2}_{u2}");
+                            
+                            oei1.Add(ch);
+                            oei2.Add(ch.Not());
+                            model.Add(u2s >= v1s + rt1).OnlyEnforceIf(oei1.ToArray());
+                            model.Add(u1s >= v2s + rt2a).OnlyEnforceIf(oei2.ToArray());
+                            disjunctions++;
+                        }
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine("Added disjunctions: " + disjunctions);
+
+        var objectives = new List<LinearExpr>();
+        foreach (var objective in problem.Objectives)
+        {
+            var t = objective.Train;
+            var u = objective.Operation;
+            var ocv = model.NewIntVar(0, upperBound, $"ocv_{t}_{u}");
+            var ocb = model.NewBoolVar($"ocb_{t}_{u}");
+            model.Add(ocv >= outerStarts[t][u] - objective.Threshold).OnlyEnforceIf(ocb);
+            var enables = new List<LinearExpr>();
+            foreach (var p in problem.Trains[t][u].Predecessors)
+                if (outerEnablers[t].ContainsKey((p, u)))
+                    enables.Add(outerEnablers[t][(p, u)]);
+
+            model.Add(ocb >= LinearExpr.Sum(enables) + problem.Trains[t][u].Predecessors.Count - enables.Count);
+            objectives.Add(objective.Coeff * ocv + objective.Increment * ocb);
+        }
+        model.Minimize(LinearExpr.Sum(objectives));
+        return (model, outerStarts, outerEnablers);
+    }
+
+    static Solution BuildAndOptimize(Problem problem, int maxTime, bool verbose)
+    {
+        var solver = new CpSolver();
+        var (model, outerStarts, outerEnablers) = BuildCpModel(problem, -1);
+        solver.StringParameters = $"num_workers:8,linearization_level:0,symmetry_level:2,max_time_in_seconds:{maxTime},log_search_progress:{verbose}";
+        var status = solver.Solve(model);
+        
+        if (status is CpSolverStatus.Feasible or CpSolverStatus.Optimal)
+        {
+            return ExtractSolution(solver, problem, outerStarts, outerEnablers);
+        }
+        return null;
+    }
+
+    private static Solution ExtractSolution(CpSolver solver, Problem problem, List<List<IntVar>> outerStarts, List<Dictionary<(int, int), LinearExpr>> outerEnablers)
+    {
+        var solution = new Solution {};
+        solution.ObjectiveValue = (int)Math.Round(solver.ObjectiveValue);
+        
+        for (int t = 0; t < problem.Trains.Count; t++)
+        {
+            for (int u = 0; u < problem.Trains[t].Count; u++)
+            {
+                if (u > 0)
+                {
+                    var enables = 0;
+                    foreach (var p in problem.Trains[t][u].Predecessors)
+                        if (outerEnablers[t].ContainsKey((p, u)))
+                        {
+                            enables += (int)solver.Value(outerEnablers[t][(p, u)]);
+                        }
+                        else
+                        {
+                            enables += 1;
+                        }
+                    if (enables < 0.5)
+                        continue;
+                }
+                int value = (int)solver.Value(outerStarts[t][u]);
+                solution.Events.Add(new Event { Operation = u, Time = value, Train = t });
+            }
+        }
+        return solution;
+    }
+
+    static void Main()
+    {
+        // var problemFile = "../../../../../displib_instances_testing/displib_instances_testing/displib_testinstances_swapping2.json";
+        var problemFile = "../../../../../displib_instances_phase1/line1_full_7.json";
+        var problem = Problem.LoadFromFile(problemFile);
+        Console.WriteLine("Building model for " + problem.Name);
+        Solution solution = BuildAndOptimize(problem, 1000, true);
+        if (solution != null)
+        {
+            var resultFile = $"results/{problem.Name}_solution.json";
+            solution.WriteToFile(resultFile);
+            Console.WriteLine("Solution found with objective: " + solution.ObjectiveValue);
+        }
+    }
+}
