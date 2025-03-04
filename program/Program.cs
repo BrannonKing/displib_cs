44f2b5ef9d4eebc6bb085d6e786abdf3bb928583
Brannon King <countprimes@gmail.com>
2025-03-04 11:51:43 -0500

    added some hints

--- program/Program.cs
+++ program/Program.cs
@@ -15,6 +15,8 @@ class Program {
 
         var outerEnablers = new List<Dictionary<(int, int), LinearExpr>>();
         var outerStarts = new List<List<IntVar>>();
+        var durationSum = 0;
+        var durationsAdded = 0;
 
         for (int t = 0; t < problem.Trains.Count; t++) {
             var train = problem.Trains[t];
@@ -36,6 +38,8 @@ class Program {
 
                 foreach (var v in train[u].Successors) {
                     int md = train[u].MinDuration;
+                    durationSum += md;
+                    durationsAdded++;
                     var added = model.Add(starts[v] >= starts[u] + md);
                     if (needsEnablers >= 0) {
                         var enabler = model.NewBoolVar($"b_{u}_{v}");
@@ -71,9 +75,11 @@ class Program {
                 }
             }
         }
+        var durationAverage = durationSum / durationsAdded;
 
         var tToChains = problem.FindResourceChains();
         var disjunctions = 0;
+        var gapped = 0;
         foreach (var chains in Tqdm.Wrap(tToChains.Values)) {
             for (int idx = 0; idx < chains.Count; idx++) {
                 var (t1, u1, v1t, rt1) = chains[idx];
@@ -85,7 +91,7 @@ class Program {
                 foreach (var (t2, u2, v2t, rt2) in chains.Skip(idx + 1)) {
                     if (t2 == t1)
                         continue;
-
+                    
                     BoolVar ch = null;
                     var v2succs = new List<int> { -1 };
                     if (problem.Trains[t2][v2t].Successors.Count > 0)
@@ -95,6 +101,7 @@ class Program {
 
                     foreach (var v1 in v1succs) {
                         foreach (var v2 in v2succs) {
+                            
                             LinearExpr v1s = v1 < 0 ? u1s + problem.Trains[t1][v1t].MinDuration : outerStarts[t1][v1];
                             var en1 = (ILiteral)outerEnablers[t1].GetValueOrDefault((v1t, v1), null);
                             var oei1 = new List<ILiteral>();
@@ -106,8 +113,39 @@ class Program {
                             var oei2 = new List<ILiteral>();
                             if (en2 != null)
                                 oei2.Add(en2);
-                            if (ch == null)
+                            
+                            // we have to filter some of these out, and we have to be smart about it.
+                            // if v1 is way before u2, and there is 20% the room necessary between them, let's just assume
+                            // that the item will get done in that order.
+                            var d1v1 = 0;
+                            var d2u2 = 0;
+                            if (v1 >= 0 && v2 >= 0) {
+                                d1v1 = distances[t1][v1];
+                                d2u2 = distances[t2][u2];
+                                // if (d2u2 - d1v1 > 10 * durationAverage + rt1) {
+                                //     var c1 = model.Add(u2s >= v1s + rt1);
+                                //     if (oei1.Count > 0)
+                                //         c1.OnlyEnforceIf(oei1.ToArray());
+                                //     gapped++;
+                                //     continue;
+                                // }
+                                //
+                                // var d1u1 = distances[t1][u1];
+                                // var d2v2 = distances[t2][v2];
+                                // if (d1u1 - d2v2 > 10 * durationAverage + rt2a) {
+                                //     var c1 = model.Add(u1s >= v2s + rt2a);
+                                //     if (oei2.Count > 0)
+                                //         c1.OnlyEnforceIf(oei2.ToArray());
+                                //     gapped++;
+                                //     continue;
+                                // }
+                            }
+
+                            if (ch == null) {
                                 ch = model.NewBoolVar($"c_{t1}_{u1}_{t2}_{u2}");
+                                // ch == 1 implies u2 after v1
+                                model.AddHint(ch, d1v1 < d2u2);
+                            }
 
                             oei1.Add(ch);
                             oei2.Add(ch.Not());
@@ -120,7 +158,7 @@ class Program {
             }
         }
 
-        Console.WriteLine("Added disjunctions: " + disjunctions);
+        Console.WriteLine("Added disjunctions: " + disjunctions + ", Gapped:" + gapped);
 
         var objectives = new List<LinearExpr>();
         foreach (var objective in problem.Objectives) {
@@ -146,7 +184,7 @@ class Program {
         var solver = new CpSolver();
         var (model, outerStarts, outerEnablers) = BuildCpModel(problem, -1);
         solver.StringParameters =
-            $"num_workers:8,linearization_level:0,symmetry_level:2,max_time_in_seconds:{maxTime},log_search_progress:{verbose}";
+            $"num_workers:8,linearization_level:0,optimize_with_core:false,symmetry_level:2,max_time_in_seconds:{maxTime},log_search_progress:{verbose}";
         var status = solver.Solve(model);
 
         if (status is CpSolverStatus.Feasible or CpSolverStatus.Optimal) {
@@ -193,10 +231,14 @@ class Program {
 
     static void Main() {
         // var problemFile = "../../../../../displib_instances_testing/displib_instances_testing/displib_testinstances_swapping2.json";
-        var problemFile = "../../../../../displib_instances_phase1/line1_critical_6.json";
+        // var problemFile = "../../../../../displib_instances_phase1/line1_critical_6.json";
+        // var problemFile = "../../../../../displib_instances_phase1/line1_full_7.json";
+        var problemFile = "../../../../../displib_instances_phase1/line3_5.json";
+        // var problemFile = "../../../../../displib_instances_phase2/line8_large_4.json";
+        // var problemFile = "../../../../../displib_instances_phase2/line3_7.json";
         var problem = Problem.LoadFromFile(problemFile);
         Console.WriteLine("Building model for " + problem.Name);
-        Solution solution = BuildAndOptimize(problem, 1000, true);
+        Solution solution = BuildAndOptimize(problem, 600, true);
         if (solution != null) {
             var resultFile = $"results/{problem.Name}_solution_ort.json";
             solution.WriteToFile(resultFile);
