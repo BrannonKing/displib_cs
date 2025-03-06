fc547b81620569dc8d8dd8f88302f2c36684bef0
Brannon King <countprimes@gmail.com>
2025-03-05 21:05:19 -0500

    Trying to make it work

--- with_rt/Program.cs
+++ with_rt/Program.cs
@@ -1,6 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Diagnostics;
 using System.Runtime.CompilerServices;
 
 namespace with_rt;
@@ -9,9 +8,11 @@ using Shared;
 using Gurobi;
 using System.Linq;
 using System;
-using TqdmSharp;
+using System.Diagnostics;
 
 class Program {
+    const int maxConsToAdd = 20000;
+
     [MethodImpl(MethodImplOptions.AggressiveOptimization)]
     static (GRBModel model, List<List<GRBVar>> outerStarts, List<Dictionary<(int, int), GRBVar>> outerEnablers,
         Dictionary<(int, int, int, int, int, int), (int, GRBTempConstr, GRBTempConstr, string)>)
@@ -25,21 +26,20 @@ class Program {
 
         var model = new GRBModel(env);
         model.ModelName = problem.Name;
+        model.Parameters.IntFeasTol = 1.0 / (upperBound + 2.0);
 
-        var outerEnablers = new List<Dictionary<(int, int), GRBVar>>();
-        var outerStarts = new List<List<GRBVar>>();
+        var outerEnablers = new List<Dictionary<(int, int), GRBVar>>(problem.Trains.Count);
+        var outerStarts = new List<List<GRBVar>>(problem.Trains.Count);
 
-        var tq1 = new Tqdm.ProgressBar(total: problem.Trains.Count, printsPerSecond: 4);
-        tq1.SetLabel("Vars and Cons for segments");
         for (int t = 0; t < problem.Trains.Count; t++) {
-            tq1.Progress(t);
             var train = problem.Trains[t];
-            var starts = new List<GRBVar>();
+            var starts = new List<GRBVar>(train.Count);
             for (int u = 0; u < train.Count; u++) {
                 int lb = distances[t][u];
                 int ub = train[u].StartUb < 0 ? upperBound : train[u].StartUb;
-                starts.Add(model.AddVar(lb, ub, 0, 'C', $"s_{t}_{u}"));
+                starts.Add(model.AddVar(lb, ub, 0, 'I', $"s_{t}_{u}"));
             }
+            model.Update();
 
             outerStarts.Add(starts);
             outerEnablers.Add(new Dictionary<(int, int), GRBVar>());
@@ -57,7 +57,7 @@ class Program {
                         var enabler = model.AddVar(0, 1, 0, 'B', $"b_{u}_{v}");
                         // enabler.BranchPriority = 100;
                         outerEnablers[t][(u, v)] = enabler;
-                        model.AddConstr(starts[v] >= starts[u] + md - (1 - enabler) * upperBound, $"aft_{u}_{v}");
+                        model.AddConstr(starts[v] >= starts[u] + md * enabler - (1 - enabler) * starts[u].LB, $"aft_{t}_{u}_{v}");
                     }
                     else {
                         model.AddConstr(starts[v] >= starts[u] + md, $"aft_{t}_{u}_{v}");
@@ -106,17 +106,12 @@ class Program {
             }
         }
 
-        tq1.Finish();
-
         var tToChains = problem.FindResourceChains();
         var disjunctions =
             new Dictionary<(int, int, int, int, int, int), (int, GRBTempConstr, GRBTempConstr, string)>();
         var consToAdd = new List<(int, GRBTempConstr, GRBTempConstr, string)>();
-        var tq2 = new Tqdm.ProgressBar(total: tToChains.Count, printsPerSecond: 4);
-        tq2.SetLabel("Build disjunctives");
 
-        foreach (var chains in tToChains.Values) {
-            tq2.Step();
+        Parallel.ForEach(tToChains.Values, new ParallelOptions{MaxDegreeOfParallelism = 8}, chains => {
             for (int idx = 0; idx < chains.Count; idx++) {
                 var (t1, u1, v1t, rt1) = chains[idx];
                 // common for t1 == v1t but not always
@@ -124,11 +119,11 @@ class Program {
                 if (problem.Trains[t1][v1t].Successors.Count > 0)
                     v1succs = problem.Trains[t1][v1t].Successors;
                 var u1s = outerStarts[t1][u1];
-                var rt1a = Math.Max(0.1, rt1);
+                var rt1a = Math.Max(1, rt1);
                 foreach (var (t2, u2, v2t, rt2) in chains.Skip(idx + 1)) {
                     if (t2 == t1)
                         continue;
-                    var rt2a = Math.Max(0.1, rt2);
+                    var rt2a = Math.Max(1, rt2);
                     GRBVar ch = null;
                     var v2succs = new List<int> { -1 };
                     if (problem.Trains[t2][v2t].Successors.Count > 0)
@@ -149,7 +144,12 @@ class Program {
                             if (en2 is not null)
                                 oei2 -= upperBound * (1 - en2);
 
-                            ch ??= model.AddVar(0, 1, 0, 'B', $"c_{t1}_{u1}_{t2}_{u2}");
+                            if (ch is null) {
+                                lock (model) {
+                                    ch ??= model.AddVar(0, 1, 0, 'B', $"c_{t1}_{u1}_{t2}_{u2}");
+                                }
+                            }
+
                             oei1 -= upperBound * (1 - ch);
                             oei2 -= upperBound * ch;
 
@@ -158,30 +158,30 @@ class Program {
                             var cname = $"dis_{t1}_{u1}_{t2}_{u2}_{v1}_{v2}";
                             var dis1 = distances[t1][u1];
                             var dis2 = distances[t2][u2];
-                            consToAdd.Add((Math.Abs(dis1 - dis2), c1, c2, cname));
-                            disjunctions[(t1, u1, v1, t2, u2, v2)] = consToAdd[^1];
+                            lock (consToAdd) {
+                                consToAdd.Add((Math.Abs(dis1 - dis2), c1, c2, cname));
+                                disjunctions[(t1, u1, v1, t2, u2, v2)] = consToAdd[^1];
+                            }
                         }
                     }
                 }
             }
-        }
-
-        tq2.Finish();
+        });
 
         consToAdd.Sort((a, b) => a.Item1.CompareTo(b.Item1));
-        for (int t = 0; t < Math.Min(50000, consToAdd.Count); t++) {
+        for (int t = 0; t < Math.Min(maxConsToAdd, consToAdd.Count); t++) {
             var (_, c1, c2, cname) = consToAdd[t];
             model.AddConstr(c1, cname + "_a");
             model.AddConstr(c2, cname + "_b");
         }
 
-        Console.WriteLine("Added up to 50k of disjunctions: " + consToAdd.Count);
+        Console.WriteLine($"Added up to {maxConsToAdd} of {consToAdd.Count} disjunctions.");
 
         var objectives = new GRBLinExpr(0);
         foreach (var objective in problem.Objectives) {
             var t = objective.Train;
             var u = objective.Operation;
-            var ocv = model.AddVar(0, upperBound, 0, 'C', $"ocv_{t}_{u}");
+            var ocv = model.AddVar(0, upperBound, 0, 'I', $"ocv_{t}_{u}");
             var ocb = model.AddVar(0, 1, 0, 'B', $"ocb_{t}_{u}");
             model.AddConstr(ocv >= outerStarts[t][u] - objective.Threshold - upperBound * (1 - ocb), $"obj_{t}_{u}");
             var enables = new GRBLinExpr(0);
@@ -209,8 +209,6 @@ class Program {
 
         private readonly Problem _problem;
 
-        private bool _wantsExit;
-
         public VerifyNoOverlapCallback(Problem problem, List<List<GRBVar>> outerStarts,
             List<Dictionary<(int, int), GRBVar>> enablers,
             Dictionary<(int, int, int, int, int, int), (int, GRBTempConstr, GRBTempConstr, string)> disjunctions) {
@@ -221,18 +219,13 @@ class Program {
 
             Console.CancelKeyPress += (sender, e) => {
                 Console.WriteLine("Ctrl+C detected, stopping optimization...");
-                _wantsExit = true;
+                Abort();
                 e.Cancel = true; // Prevent immediate process termination
             };
         }
 
         [MethodImpl(MethodImplOptions.AggressiveOptimization)]
         protected override void Callback() {
-            if (_wantsExit) {
-                Abort();
-                return;
-            }
-
             if (where == GRB.Callback.MIPSOL) {
                 try {
                     // key is res_name, value is start_time, stop_time, train, start_operation, end_operation
@@ -290,25 +283,26 @@ class Program {
         }
     }
 
-    static Solution BuildAndOptimize(Problem problem, int maxTime, bool verbose) {
+    static Solution BuildAndOptimize(Problem problem, Stopwatch sw, int maxTime, bool verbose) {
         var (model, outerStarts, outerEnablers, disjunctions) = BuildCpModel(problem, -1);
         GC.Collect(); // need all the RAM we can get
         model.Parameters.Threads = 8;
+        model.Parameters.Method = 1;
+        // model.Parameters.ConcurrentMethod = 3; // don't run Barrier method
         model.Parameters.MemLimit = 32;
         // model.Parameters.Cuts = 0;
-        // model.Parameters.Method = 2;
         // model.Parameters.Crossover = 1;
         // model.Parameters.Method = 1;
         // model.Parameters.Crossover = 0;
-        model.Parameters.TimeLimit = maxTime;
         model.Parameters.LogToConsole = verbose ? 1 : 0;
 
-        if (disjunctions.Count > 50000) {
+        if (disjunctions.Count > maxConsToAdd) {
             model.Parameters.LazyConstraints = 1;
             var cb = new VerifyNoOverlapCallback(problem, outerStarts, outerEnablers, disjunctions);
             model.SetCallback(cb);
-        }
+        } 
 
+        model.Parameters.TimeLimit = maxTime - Math.Ceiling(sw.Elapsed.TotalSeconds);
         model.Optimize();
 
         if (model.SolCount > 0) {
@@ -364,16 +358,18 @@ class Program {
     }
 
     static void Main() {
+        var sw = Stopwatch.StartNew();
         // this approach fails because of a faulty assumption:
         // you can repair a failed solution by changing start times.
         // cutting the disjunctions instead makes unnecessary changes.
         // var problemFile = "../../../../../displib_instances_testing/displib_instances_testing/displib_testinstances_headway1.json";
-        // var problemFile = "../../../../../displib_instances_phase1/line1_full_7.json";
-        // var problemFile = "../../../../../displib_instances_phase1/line1_critical_6.json";
-        var problemFile = "../../../../../displib_instances_phase2/line8_small_2.json";
+        var problemFile = "../../../../../displib_instances_phase1/line1_full_7.json";
+        // var problemFile = "../../../../../displib_instances_phase1/line1_full_0.json";
+        // var problemFile = "../../../../../displib_instances_phase1/line1_critical_3.json";
+        // var problemFile = "../../../../../displib_instances_phase2/line8_small_2.json";
         var problem = Problem.LoadFromFile(problemFile);
         Console.WriteLine("Building model for " + problem.Name);
-        var solution = BuildAndOptimize(problem, 1000, true);
+        var solution = BuildAndOptimize(problem, sw, 598, true);
         if (solution != null) {
             var resultFile = $"results/{problem.Name}_solution.json";
             solution.WriteToFile(resultFile);
