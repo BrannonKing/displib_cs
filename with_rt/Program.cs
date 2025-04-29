4077067c4352607578c2541547e66ec612c89bf9
Brannon King <countprimes@gmail.com>
2025-04-29 19:47:28 -0400

    changed back to continuous

--- with_rt/Program.cs
+++ with_rt/Program.cs
@@ -59,8 +59,8 @@ class Program {
 
         var model = new GRBModel(env);
         model.ModelName = problem.Name;
-        model.Parameters.IntFeasTol = .47 / upperBound;
-        // model.Parameters.IntegralityFocus = 1;
+        // model.Parameters.IntFeasTol = .47 / (upperBound * 0.25);
+        model.Parameters.IntegralityFocus = 1;
 
         var outerEnablers = new Dictionary<int, Dictionary<(int, int), GRBVar>>(problem.Trains.Count);
         var outerStarts = new Dictionary<int, GRBVar[]>(problem.Trains.Count);
@@ -73,7 +73,7 @@ class Program {
             for (int u = 0; u < train.Count; u++) {
                 int lb = distances[t][u];
                 int ub = train[u].StartUb < 0 ? upperBound : train[u].StartUb;
-                starts[u] = model.AddVar(lb, ub, 0, 'I', $"s_{t}_{u}");
+                starts[u] = model.AddVar(lb, ub, 0, 'C', $"s_{t}_{u}");
                 // starts[u].Start = distances[t][u];
             }
             model.Update();
@@ -200,7 +200,7 @@ class Program {
             if (mask.Contains(t))
                 continue;
             var u = objective.Operation;
-            var ocv = model.AddVar(0, upperBound, 0, 'I', $"ocv_{t}_{u}");
+            var ocv = model.AddVar(0, upperBound, 0, 'C', $"ocv_{t}_{u}");
             var ocb = model.AddVar(0, 1, 0, 'B', $"ocb_{t}_{u}");
             model.AddConstr(ocv >= outerStarts[t][u] - objective.Threshold - upperBound * (1 - ocb), $"obj_{t}_{u}");
             var enables = new GRBLinExpr(0);
@@ -540,7 +540,7 @@ class Program {
         // cutting the disjunctions instead makes unnecessary changes.
         // var problemFile = "../../../../../displib_instances_testing/displib_instances_testing/displib_testinstances_headway1.json";
         // var problemFile = "../../../../../displib_instances_phase1/line1_full_7.json";
-        var problemFile = args.Length > 0 ? args[0] : "../../../../../displib_instances_phase1/line1_full_4.json";
+        var problemFile = args.Length > 0 ? args[0] : "../../../../../displib_instances_phase1/line1_critical_6.json";
         // var problemFile = "../../../../../displib_instances_phase2/line3_8.json";
         // var problemFile = "../../../../../displib_instances_phase1/line3_5.json";
         // var problemFile = "../../../../../displib_instances_phase1/line2_headway_3.json";
